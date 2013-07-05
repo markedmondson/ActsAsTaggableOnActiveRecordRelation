784b408e15acc1ac54d56ffa44abe486ea9fa876
markedmondson <mark.edmondson@gmail.com>
2013-07-04 17:25:45 -0700

    Update acts_as_taggable_on_active_record_relation.rb
    Switch to extendable module and use activerecord-import to speed up multiple inserts.

--- acts_as_taggable_on_active_record_relation.rb
+++ acts_as_taggable_on_active_record_relation.rb
@@ -1,26 +1,29 @@
-module ActsAsTaggableOnActiveRecordRelation
+module ActsAsTaggableOnBulk
 
   # Add a list of tags to an ActiveRecord::Relation
   def add_tags(tags,context=nil)
     return self if tags.empty?
     data = prepare_transaction_for_tagging(tags, context)
     ActsAsTaggableOn::Tagging.transaction do
-      data[:resources_ids].each do |resource_id|
+      columns = [:tag_id, :taggable_id, :taggable_type, :context]
+
+      all_values = data[:resources_ids].reduce([]) do |values, resource_id|
         data[:tags_ids].each do |tag_id|
-          values  = [data[:resource_name].inspect,resource_id, tag_id,data[:context_value]].join(',')
-          request = "INSERT INTO taggings (taggable_type, taggable_id, tag_id, context) VALUES (#{values}) ON DUPLICATE KEY UPDATE id=id"
-          ActsAsTaggableOn::Tagging.connection.execute(request)
+          values << [tag_id, resource_id, data[:resource_name], data[:context_value]]
         end
+        values
       end
+
+      ActsAsTaggableOn::Tagging.import(columns, all_values, validate: false, on_duplicate_key_update: columns)
     end
     self
   end
-  
+
   # Add one tag to an ActiveRecord::Relation
   def add_tag(tag, context=nil)
     add_tags([tag], context)
   end
-  
+
   # Remove a list of tags from an ActiveRecord::Relation
   def remove_tags(tags,context=nil)
     return self if tags.empty?
@@ -33,14 +36,14 @@ module ActsAsTaggableOnActiveRecordRelation
     }).delete_all
     self
   end
-  
+
   # Remove one tag from an ActiveRecord::Relation
   def remove_tag(tag, context=nil)
     remove_tags([tag], context)
   end
-  
+
   private
-  
+
   # Filter an array and returns only ActsAsTaggableOn::Tag objects
   def filter_list_to_tags(tag_list)
     strings = tag_list.reject{|t| t unless t.kind_of?(String) }
@@ -48,17 +51,16 @@ module ActsAsTaggableOnActiveRecordRelation
     tags   += ActsAsTaggableOn::Tag.find_or_create_all_with_like_by_name(strings) unless strings.empty?
     tags
   end
-  
+
   # Filter the given array and returns only ActsAsTaggableOn::Tag list
   def prepare_transaction_for_tagging(tags, context=nil)
     filtered_tags = filter_list_to_tags(tags)
     {
       tags_ids:       filtered_tags.map{|item| item.id},
-      resources_ids:  self.select('id').map{|item| item.id},
+      resources_ids:  self.pluck("#{self.table_name}.id"),
       resource_name:  self.klass.to_s,
-      context_value:  context_value = context ? context.inspect : 'NULL'
+      context_value:  context_value = context
     }
   end
-    
+
 end
-ActiveRecord::Relation.send(:include, ActsAsTaggableOnActiveRecordRelation)
